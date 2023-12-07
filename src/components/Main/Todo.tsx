3bc5c263b32fe912da9bb8441ea4222a7f949291
Aquamoris <pestryak.nikita.dev@gmail.com>
2023-12-07 23:32:50 +1000

    Make Todo List

---
+++ src/components/Main/Todo.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import type {Todo} from "../../utils/types/Todo.type.ts";
+import {useAppDispatch} from "../../hooks/storeHooks/hooks.ts";
+import {removeTodo, toggleTodoComplete} from "../../store/todosSlice.tsx";
+import cn from 'classnames';
+import style from './Todo.module.scss';
+
+const Todo:React.FC<Todo> = ({id, body, completed}) => {
+    const dispatch = useAppDispatch();
+
+    return (
+        <div className={style.todo}>
+            {/*<input className={style.complete} onChange={() => dispatch(toggleTodoComplete(id))} type="checkbox" checked={completed}/>*/}
+            <div className={cn(style.body, {
+                [style.bodyCompleted]: completed
+            })}
+                 onClick={() => dispatch(toggleTodoComplete(id))}
+            >{body}</div>
+            <div className={style.remove} onClick={() => dispatch(removeTodo(id))}>
+                <div className={cn(style.icon, {
+                    [style.iconCompleted]: completed
+                })}>X</div>
+            </div>
+        </div>
+    );
+};
+
+export default Todo;
